41c8e797047052ddf4e6ffb385264234e3a05685
6equal0 <123716014+6equal0@users.noreply.github.com>
2024-08-05 11:39:27 +0900

    Title: 두 숫자의 차의 최솟값, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240805/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, temp, min = INT_MAX;
+    cin >> n;
+    int *arr = new int[n];
+
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    for(int i = 1; i < n; i++){
+        temp = arr[i] = arr[i-1];
+
+        if(temp < min){
+            min = temp;
+        }
+
+        if(temp == 0){
+            break;
+        }
+    }
+
+    cout << min;
+
+    return 0;
+}
